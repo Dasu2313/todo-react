3b216a3ea0e6ac8ef3f54f7c0eedddbe486b0bbf
Dasu2313 <dasu23@mail.ru>
2025-05-19 19:22:56 +0500

    fix deploy again

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: 'https://dasu23.github.io/todo-react/',
+  base: 'https://dasu2313.github.io/todo-react/',
   plugins: [react()],
 })
